6a3d2fbd4d65e5260b06de8718e53dc83c56563c
Qaizx <kaothom@gmail.com>
2020-12-15 15:52:39 +0700

    "lab5_1.cpp completed"

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
 using namespace std;
 
+int x=1, even = 0, old = 0;
+
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+    while(x!=0){
+
+        if(x != 0){
+        cout << "Enter an integer: ";
+        cin >> x;
+        cout << "\n";
+        if(x %2 == 0){
+            even++;
+        }else{
+            old++;
+
+        }
+    }
+
+    }
+
+
+    cout << "#Even numbers = " << even-1 << endl;
+    cout << "#Odd numbers = " << old;
     return 0;
 }
